524c77a970b074504baeb72e9f647b040064b5c4
ysf12374 <47970186+ysf12374@users.noreply.github.com>
2021-03-17 00:15:00 +0100

    Update single.py

--- scrapealong/tripadvisor/hotels/single.py
+++ scrapealong/tripadvisor/hotels/single.py
@@ -3,6 +3,7 @@
 from ...fetch import SlowFetcher
 from . import scrape
 from ..base import BasePicker
+from ..base import Browser as Browser_
 
 import asyncio
 from tqdm import tqdm
@@ -12,6 +13,12 @@ class Picker(BasePicker):
 
     async def run(self, urls):
         fetcher = SlowFetcher()
-        geo_and_props = await asyncio.gather(*[fetcher.browse(url) for url in tqdm(urls)])
+        geo_and_props = await asyncio.gather(*[fetcher.fetch(url) for url in tqdm(urls)])
 
-        return map(lambda args: (args[0], scrape.details(*args[1:]), args[1],), geo_and_props)
+        # return map(lambda args: (args[0], scrape.details(*args[1:]), args[1],), geo_and_props)
+        return map(lambda pp: scrape.details(pp[0], pp[1]), zip(geo_and_props,urls))
+
+class Browser(Browser_):
+
+    def _load(self):
+        self.sid, self.lon_lat, self.details, self.warnings = scrape.details(self.body,self.url)
